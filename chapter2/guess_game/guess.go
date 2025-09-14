2c06b9171a33752fbbad7edca2c81909b08fa132
Folombas <folombas@yandex.ru>
2025-09-14 23:18:45 +0300

    guess simple console game

--- chapter2/guess_game/guess.go
+++ chapter2/guess_game/guess.go
@@ -1,3 +1,4 @@
+// Guess - Игра, в которой игрок должен угадать случайное число.
 package main
 
 import (
@@ -17,24 +18,34 @@ func main() {
 	target := rand.Intn(100) + 1
 	fmt.Println("Я выбрал случайное число от 1 до 100.")
 	fmt.Println("Сможете угадать это число?")
-	fmt.Println(target)
 
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Make a guess: ")
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if guess < target {
-		fmt.Println("Oops. Your guess was LOW.")
-	} else if guess > target {
-		fmt.Println("Oops. Your guess was HIGH.")
-	}
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
 
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
+	}
+	if !success {
+		fmt.Println("Sorry, you didn`t guess my number. It was:", target)
+	}
 }
