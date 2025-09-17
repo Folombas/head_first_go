d6856eaf3f33566404a8832fcc40b0c148a77a78
Folombas <folombas@yandex.ru>
2025-09-18 00:31:11 +0300

    Examples of the most popular formatting verbs

--- chapter3/practics_formatting/practics.formatting.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Printf("%s имеет обычно %d колеса", "Автомашина", 4)
-}
---
+++ chapter3/practics_formatting/practics_formatting.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("%s имеет обычно %d колеса\n", "Автомашина", 4)
+
+	fmt.Printf("A float: %f\n", 3.1415)
+	fmt.Printf("An integer: %d\n", 15)
+	fmt.Printf("A string: %s\n", "hello")
+	fmt.Printf("A boolean: %t\n", false)
+	fmt.Printf("Values: %v %v %v\n", false)
+	fmt.Printf("Values: %#v %#v %#v\n", 1.2, "\t", true)
+	fmt.Printf("Types: %T %T %T\n", 1.2, "\t", true)
+	fmt.Printf("Percent sign: %%\n")
+}
